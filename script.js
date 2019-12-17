e86b835f7302d034d31ea2595c30ba5e8ce7313e
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-12-17 15:23:12 -0300

    add edit form to update cat info

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ const Cat = () => {
 
 const CatModel = numberOfCats => {
   let cats = [];
+  let currentSelectedCat;
 
   return {
     loadCats: () => {
@@ -42,13 +43,20 @@ const CatModel = numberOfCats => {
       }
       return cats;
     },
-    incrementCatClickCounter: name => {
-      cats.map(cat => {
-        if (cat.getName() === name) {
-          const currentCounter = cat.getClickCounter();
-          cat.setClickCounter(currentCounter + 1);
-        }
-      });
+    incrementCatClickCounter: () => {
+      const currentCounter = currentSelectedCat.getClickCounter();
+      currentSelectedCat.setClickCounter(currentCounter + 1);
+    },
+    getCurrentSelectedCat: () => {
+      return currentSelectedCat;
+    },
+    setCurrentSelectedCat: cat => {
+      currentSelectedCat = cat;
+    },
+    updateCurrentSelectedCat: updatedCat => {
+      currentSelectedCat.setName(updatedCat.getName());
+      currentSelectedCat.setImagePath(updatedCat.getImagePath());
+      currentSelectedCat.setClickCounter(updatedCat.getClickCounter());
     }
   };
 };
@@ -57,22 +65,33 @@ const CatModel = numberOfCats => {
 
 const CatController = () => {
   const catModel = CatModel(6);
+  const catThumbnailView = CatThumbnailView();
 
   return {
     getCats: () => {
       return catModel.loadCats();
     },
-    incrementCatClickCounter: catName => {
-      catModel.incrementCatClickCounter(catName);
+    getSelectedCat: () => {
+      return catModel.getCurrentSelectedCat();
+    },
+    setSelectedCat: cat => {
+      catModel.setCurrentSelectedCat(cat);
+    },
+    incrementCatClickCounter: () => {
+      catModel.incrementCatClickCounter();
+    },
+    updateCurrentSelectedCat: updatedCat => {
+      catModel.updateCurrentSelectedCat(updatedCat);
     },
     init: () => {
-      CatThumbnailView().createThumbnailDisplayList();
+      catThumbnailView.renderThumbnailList();
     }
   };
 };
 
-// VIEW ------------------------------------------------------------------------
+// VIEWS -----------------------------------------------------------------------
 
+// HELPER FUNTION --------------------------------------------------------------
 const createImageElement = imagePath => {
   const img = document.createElement("img");
   img.src = imagePath;
@@ -80,9 +99,12 @@ const createImageElement = imagePath => {
   return img;
 };
 
+// NAVBAR ----------------------------------------------------------------------
 const CatThumbnailView = () => {
+  const editForm = document.querySelector("footer.form-container");
+
   return {
-    createThumbnailDisplayList: () => {
+    renderThumbnailList: () => {
       const catNavigationBar = document.querySelector("div.thumbnail-display");
       const cats = catController.getCats();
 
@@ -90,7 +112,9 @@ const CatThumbnailView = () => {
         const catThumbImage = createImageElement(cat.getImagePath());
 
         catThumbImage.addEventListener("click", () => {
-          CatView().createMainImageDisplay(cat);
+          editForm.style.display = "none";
+          catController.setSelectedCat(cat);
+          CatDisplayView().renderMainImageDisplay();
         });
 
         catNavigationBar.appendChild(catThumbImage);
@@ -99,33 +123,87 @@ const CatThumbnailView = () => {
   };
 };
 
-const CatView = () => {
+// EDIT FORM -------------------------------------------------------------------
+const CatEditFormView = () => {
+  const selectedCat = catController.getSelectedCat();
+
+  const editForm = document.querySelector("footer.form-container");
+  editForm.addEventListener("submit", event => {
+    event.preventDefault();
+  });
+
+  const [catNameInput, catImageURL, catClicks] = editForm.querySelectorAll(
+    "form input"
+  );
+
+  const cancelButton = editForm.querySelector("form button#cancel");
+  cancelButton.addEventListener("click", () => {
+    editForm.style.display = "none";
+  });
+
+  const saveButton = editForm.querySelector("form button#save");
+  saveButton.addEventListener("click", () => {
+    const updatedCat = Cat();
+    updatedCat.setName(catNameInput.value);
+    updatedCat.setImagePath(catImageURL.value);
+    updatedCat.setClickCounter(parseInt(catClicks.value, 10));
+    catController.updateCurrentSelectedCat(updatedCat);
+    editForm.style.display = "none";
+    CatDisplayView().renderMainImageDisplay();
+  });
+
   return {
-    createMainImageDisplay: cat => {
-      const catDisplay = document.querySelector("div.cat-image-display");
-      catDisplay.innerHTML = "";
-
-      const catDisplayName = document.createElement("p");
-      catDisplayName.innerHTML = cat.getName();
-
-      const catDisplayImage = createImageElement(cat.getImagePath());
-      catDisplayImage.addEventListener("click", () => {
-        catController.incrementCatClickCounter(cat.getName());
-        const clickCounterDisplay = document.querySelector(
-          "div.cat-image-display span"
-        );
-        clickCounterDisplay.innerHTML = `Number of clicks: ${cat.getClickCounter()}`;
-      });
-
-      const clickCounterDisplay = document.createElement("span");
-      clickCounterDisplay.innerHTML = `Number of clicks: ${cat.getClickCounter()}`;
-
-      catDisplay.appendChild(catDisplayName);
-      catDisplay.appendChild(catDisplayImage);
-      catDisplay.appendChild(clickCounterDisplay);
+    showCatEditForm: () => {
+      catNameInput.value = selectedCat.getName();
+      catImageURL.value = selectedCat.getImagePath();
+      catClicks.value = selectedCat.getClickCounter();
+      editForm.style.display = "flex";
     }
   };
 };
 
+// MAIN DISPLAY ----------------------------------------------------------------
+const CatDisplayView = () => {
+  const selectedCat = catController.getSelectedCat();
+  const catDisplay = document.querySelector("main.cat-image-display");
+
+  const handleDisplayImageClick = () => {
+    catController.incrementCatClickCounter();
+    const catClickCounterDisplay = document.querySelector(
+      "main.cat-image-display span"
+    );
+    catClickCounterDisplay.innerHTML = `Number of clicks: ${selectedCat.getClickCounter()}`;
+  };
+
+  const handleAdminButtonClick = () => {
+    CatEditFormView().showCatEditForm();
+  };
+
+  return {
+    renderMainImageDisplay: () => {
+      catDisplay.innerHTML = "";
+
+      const catDisplayName = document.createElement("p");
+      catDisplayName.innerHTML = selectedCat.getName();
+
+      const catDisplayImage = createImageElement(selectedCat.getImagePath());
+      catDisplayImage.addEventListener("click", handleDisplayImageClick);
+
+      const catClickCounterDisplay = document.createElement("span");
+      catClickCounterDisplay.innerHTML = `Number of clicks: ${selectedCat.getClickCounter()}`;
+
+      const adminButton = document.createElement("button");
+      adminButton.addEventListener("click", handleAdminButtonClick);
+      adminButton.innerHTML = "Admin";
+
+      catDisplay.appendChild(catDisplayName);
+      catDisplay.appendChild(catDisplayImage);
+      catDisplay.appendChild(catClickCounterDisplay);
+      catDisplay.appendChild(adminButton);
+    }
+  };
+};
+
+// START THE APP ---------------------------------------------------------------
 const catController = CatController();
 catController.init();
